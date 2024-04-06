22facf62729ebf0bc039f0cdafda1c368aaf98c4
Denys Revutskyi <denys.msi04@gmail.com>
2024-04-06 18:50:39 +0200

    add events calendar and install depend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import {importProvidersFrom} from '@angular/core';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {MatNativeDateModule} from '@angular/material/core';
+
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    importProvidersFrom(MatNativeDateModule)
   ]
 };
--- frontend/src/app/components/events/events.component.ts
+++ frontend/src/app/components/events/events.component.ts
@@ -1,24 +1,37 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MAT_DATE_LOCALE} from '@angular/material/core';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideMomentDateAdapter} from '@angular/material-moment-adapter';
+import 'moment/locale/es';
 
 @Component({
   selector: 'app-events',
+  imports: [MatCardModule, MatDatepickerModule],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'es-ES'},
+    provideNativeDateAdapter(),
+    provideMomentDateAdapter(),
+  ],
   standalone: true,
-  imports: [],
   template: `
-    <main>
-      <div class="bg-image h-screen">
-        <div class="flex justify-center items-center h-screen bg-image">
-          <div class="bg-blue-400 rounded-lg overflow-hidden shadow-md p-8">
-            <div class="max-w-screen-lg mx-auto p-5">
-              <h1>Eventos bien</h1>
+    <div class="flex justify-center p-5">
+      <div class="bg-blue-400 rounded-lg overflow-hidden shadow-md p-8">
+        <div class="max-w-screen-lg mx-auto p-5">
+          <div class="flex justify-center items-center">
+            <div class="max-w-screen-lg mx-auto p-5 text-white">
+              <h1 class="text-3xl font-bold mb-5">Nuestros eventos:</h1>
+              <mat-card class="tam">
+                <mat-calendar [(selected)]="selected"></mat-calendar>
+              </mat-card>
+              <p>Selected date: {{selected}}</p>
             </div>
           </div>
         </div>
       </div>
-    </main>
+    </div>
   `,
-  styles: ``
+  styles: `.tam { width: 600px; }`
 })
-export class EventsComponent {
-
-}
+export class EventsComponent { selected: Date | null = new Date; }
