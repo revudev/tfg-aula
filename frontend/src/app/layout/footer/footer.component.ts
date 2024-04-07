e2191d97034c24cc87785e5fccabdd2ec6c11a94
Denys Revutskyi <denys.msi04@gmail.com>
2024-04-07 15:15:47 +0200

    add footer routerLink

--- frontend/src/app/layout/footer/footer.component.ts
+++ frontend/src/app/layout/footer/footer.component.ts
@@ -5,10 +5,44 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <footer class="bg-gray-800 text-white py-4">
-      <div class="container mx-auto flex justify-center items-center">
-        <p class="text-center">© 2024 Todos los derechos reservados</p>
-      </div>
+    <footer class="w-full bg-blue-500 py-8 flex flex-col items-center">
+      <!-- <ul class="flex flex-wrap justify-center mb-4">
+        <li class="list-none">
+          <a href="#" class="text-white text-2xl mr-4 transition duration-500 transform hover:-translate-y-1">
+            <ion-icon name="logo-facebook"></ion-icon>
+          </a>
+        </li>
+        <li class="list-none">
+          <a href="#" class="text-white text-2xl mr-4 transition duration-500 transform hover:-translate-y-1">
+            <ion-icon name="logo-twitter"></ion-icon>
+          </a>
+        </li>
+        <li class="list-none">
+          <a href="#" class="text-white text-2xl mr-4 transition duration-500 transform hover:-translate-y-1">
+            <ion-icon name="logo-linkedin"></ion-icon>
+          </a>
+        </li>
+        <li class="list-none">
+          <a href="#" class="text-white text-2xl transition duration-500 transform hover:-translate-y-1">
+            <ion-icon name="logo-instagram"></ion-icon>
+          </a>
+        </li>
+      </ul> -->
+      <ul class="flex flex-wrap justify-center mb-4">
+        <li class="list-none">
+          <a routerLink="/" routerLinkActive="active" aria-current="page" class="text-white text-lg mr-4 transition duration-500 opacity-75 hover:opacity-100">Inicio</a>
+        </li>
+        <li class="list-none">
+          <a routerLink="/contact" routerLinkActive="active" aria-current="page" class="text-white text-lg mr-4 transition duration-500 opacity-75 hover:opacity-100">Contactanos</a>
+        </li>
+        <li class="list-none">
+          <a routerLink="/iniciarrhh" routerLinkActive="active" aria-current="page" class="text-white text-lg mr-4 transition duration-500 opacity-75 hover:opacity-100">Iniciativa</a>
+        </li>
+        <li class="list-none">
+          <a routerLink="/gestions" routerLinkActive="active" aria-current="page" class="text-white text-lg mr-4 transition duration-500 opacity-75 hover:opacity-100">Gestiones</a>
+        </li>
+      </ul>
+      <p class="text-white text-lg font-light">&copy;2024 Todos los derechos reservados| Denys Revutskyi</p>
     </footer>
   `,
   styles: ``
